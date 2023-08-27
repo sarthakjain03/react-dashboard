0fc328ef7c0644b25a5a6778c4141ed8c71337c8
Sarthak Jain <jainsarthak2003@gmail.com>
2023-08-27 12:50:08 +0530

    added revenue sec

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BsCurrencyDollar } from 'react-icons/bs'
-import { GoPrimitiveDot } from 'react-icons/go'
+import { GoDotFill } from 'react-icons/go'
 
 import { Stacked, Pie, Button, SparkLine } from '../components'
 import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy'
@@ -47,7 +47,54 @@ const Ecommerce = () => {
             </div>
           ))}
         </div>
+      </div>
 
+      <div className='flex gap-10 flex-wrap justify-center'>
+            <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+              <div className='flex justify-between'>
+                <p className='font-semibold text-xl'>Revenue Updates</p>
+                <div className='flex items-center gap-4'>
+                  <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                    <span><GoDotFill /></span>
+                    <span>Expense</span>
+                  </p>
+                  <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                    <span><GoDotFill /></span>
+                    <span>Budget</span>
+                  </p>
+                </div>
+              </div>
+              <div className='mt-10 flex flex-wrap justify-center gap-10'>
+                <div className='border-r-1 border-color m-4 pr-10'>
+                  <div>
+                    <p>
+                      <span className='text-3xl font-semibold'>$93,890</span>
+                      <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3'>23%</span>
+                    </p>
+                    <p className='text-gray-500 mt-1'>Budget</p>
+                  </div>
+                  <div className='mt-8'>
+                    <p>
+                      <span className='text-3xl font-semibold'>$45,668</span>
+                    </p>
+                    <p className='text-gray-500 mt-1'>Expense</p>
+                  </div>
+
+                  <div className='mt-5'>
+                    <SparkLine
+                      currentColor="blue"
+                      id="line-sparkline"
+                      type="Line"
+                      height="80px"
+                      width="250px"
+                      color="blue"
+                      data={SparklineAreaData}
+                    />
+                  </div>
+
+                </div>
+              </div>
+            </div>
       </div>
     </div>
   )
