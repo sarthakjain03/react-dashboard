cac803b725a6ddbb784bcc5b474dd09f6c8bcade
Sarthak Jain <jainsarthak2003@gmail.com>
2023-08-27 15:37:35 +0530

    revenue sec done

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -91,7 +91,18 @@ const Ecommerce = () => {
                       data={SparklineAreaData}
                     />
                   </div>
+                  <div className='mt-10'>
+                    <Button
+                      color="white"
+                      bgColor="blue"
+                      text="Download Report"
+                      borderRadius="10px"
+                    />
+                  </div>
 
+                </div>
+                <div>
+                  <Stacked width="320px" height="360px" />
                 </div>
               </div>
             </div>
