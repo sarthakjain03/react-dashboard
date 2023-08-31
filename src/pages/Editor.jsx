a7d9a196496f19a75df28da622f5476111639f07
Sarthak Jain <jainsarthak2003@gmail.com>
2023-08-31 17:02:52 +0530

    apps done

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, Toolbar, RichTextEditorComponent } from '@syncfusion/ej2-react-richtexteditor'
+
+import { EditorData } from '../data/dummy'
+import { Header } from '../components'
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 rounded-3xl bg-white'>
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Image, Link, Toolbar, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
