4f004a00647424f64383e4a1928f27958a73bb18
Sarthak Jain <jainsarthak2003@gmail.com>
2023-09-04 11:23:19 +0530

    ecommerce banner fixed

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -13,7 +13,7 @@ const Ecommerce = () => {
   return (
     <div className='mt-12'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-full p-8 pt-9 m-8 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
           <div className='flex justify-between items-center'>
             <div>
               <p className='font-bold text-gray-400'>Earnings</p>
@@ -30,8 +30,10 @@ const Ecommerce = () => {
             />
           </div>
         </div>
-
-        <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
+      </div>
+        
+      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
+        <div className='flex m-3 flex-wrap justify-center gap-2 items-center'>
           {earningData.map((item) => (
             <div key={item.title} className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'>
               <button type="button" style={{color: item.iconColor, backgroundColor: item.iconBg}} className='text-2xl rounded-full p-4 hover:drop-shadow-xl'>
